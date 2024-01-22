bc4a1453c6a07e37de01ee7dae9c3eb55a9512f1
davidochoadev <davidochoa.dev@gmail.com>
2024-01-22 23:20:26 +0100

    ✅ Feat: Created mobile menu

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		colors: {
+			'main':'#1a1520',
+			'activate': '#fff',
+			'white':'#ffffff',
+			'not-activate': '#d4d4d4e6',
+			'border-color': '#262626',
+			'form-not-activate': '#c4c4c4',
+			'gold':'#d5b33d',
+			'zinc':'rgb(113 113 122)'
+		},
+		extend: {
+			backgroundImage: {
+				'hero-pattern': "url('/src/assets/studio.webp')",
+			 },
+			 inset: {
+				'center-socials': 'calc(50% - 81px)',
+			 },
+			 height: {
+				'mid': 'calc(100% - 150px)',
+				'mid2': 'calc(90vh - 10vh)'
+			 },
+		},
 	},
 	plugins: [],
 }
