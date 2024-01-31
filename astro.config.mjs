102edd4171bb95f6ba3aa5a125dbd9de8a84fbb7
davidochoadev <davidochoa.dev@gmail.com>
2024-01-31 20:20:26 +0100

    🛠 Fix: removed minify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
-import minify from "astro-min";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown(), minify()]
+  integrations: [tailwind(), partytown()]
 });
