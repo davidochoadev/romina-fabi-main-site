1920a18701f310b5fad89361c179c8517e92438e
davidochoadev <davidochoa.dev@gmail.com>
2024-01-31 20:17:16 +0100

    ✅ Feat: added minify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import minify from "astro-min";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown()]
+  integrations: [tailwind(), partytown(), minify()]
 });
